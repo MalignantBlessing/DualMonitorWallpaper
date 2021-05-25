8dc21f01b8c13988ff2b2de70357123fc520ff80
Matthew Jonas <84788497+MalignantBlessing@users.noreply.github.com>
2021-05-25 16:31:00 +0100

    GitHub test.

--- DualMonitorWallpaper/DMW.cs
+++ DualMonitorWallpaper/DMW.cs
@@ -43,7 +43,8 @@ namespace DualMonitorWallpaper
         string monitor;
         string wallpaperSavePath;
         #endregion
-
+        
+        // GitHub test.
         public DMW()
         {
             InitializeComponent();
